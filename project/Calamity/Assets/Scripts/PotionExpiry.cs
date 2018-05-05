9e4f899537df8177e4b700ff80d6bfb96030b697
FBI\Антон <Anton-nabokin1997@yandex.ru>
2018-05-06 02:33:12 +0300

    Add throwing bottle with effect

---
+++ project/Calamity/Assets/Scripts/PotionExpiry.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PotionExpiry : MonoBehaviour {
+
+    public ParticleSystem boom;
+    public MeshRenderer mesh;
+    public Rigidbody rb;
+    public bool throwed = false;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(!other.CompareTag("Player")&!throwed)
+        {
+            boom.Play();
+            mesh.enabled = false;
+            throwed = true;
+            rb.isKinematic = true;
+        }
+    }
+    private void Update()
+    {
+        if(!boom.IsAlive() & throwed)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
